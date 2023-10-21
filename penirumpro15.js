25258662c230963d43e29607c6e6003bdaa7caaa
whygithub7 <58257405+whygithub7@users.noreply.github.com>
2023-10-21 21:10:47 +0300

    Update penirumpro15.js

--- penirumpro15.js
+++ penirumpro15.js
@@ -1,6 +1,5 @@
 console.log("Penirum Pro15 - ok")
 
- setTimeout(function() {
-            // Указать URL, на который вы хотите перенаправить пользователя
-            window.location.href = 'https://example.com';
-        }, 40000);
+  setTimeout(function(){
+    window.location.href = 'https://example.com';
+}, 40000);
